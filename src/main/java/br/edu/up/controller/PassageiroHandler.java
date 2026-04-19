a08d216b21cdd94393b37c3b8b8d884ab1629473
PedroWernek <pedrowcnetto@gmail.com>
2026-04-18 22:30:48 -0300

    update: explicando algumas funções

--- src/main/java/br/edu/up/controller/PassageiroHandler.java
+++ src/main/java/br/edu/up/controller/PassageiroHandler.java
@@ -14,6 +14,8 @@ public class PassageiroHandler implements HttpHandler {
     private final ObjectMapper mapper = new ObjectMapper();
     private final PassageiroService service = new PassageiroService();
 
+
+    //vendo qual requisição foi feita para que seja feita a operação
     @Override
     public void handle(HttpExchange exchange) throws IOException {
 
@@ -34,19 +36,37 @@ public class PassageiroHandler implements HttpHandler {
 
     }
 
+    /**Listando TODOS os passagerios da Tabela passageiro
+     * não esqueça que a operação que faz a comunicação com o banco está no repository
+     */
     private void listar(HttpExchange exchange) throws Exception {
         List<Passageiro> produtos = service.listarPassageiros();
-        String json = mapper.writeValueAsString(produtos);
-        enviar(exchange, json);
+
+        String json = mapper.writeValueAsString(produtos);//escrevendo a lista de passageiros como um json
+
+        enviar(exchange, json); //<- *
     }
 
+    /** Criando um novo passageiro no banco
+     *
+     *
+     */
     private void criar(HttpExchange exchange) throws Exception {
         Passageiro p = mapper.readValue(exchange.getRequestBody(), Passageiro.class);
-        Passageiro criado = service.criarPassageiro(p);
-        String json = mapper.writeValueAsString(criado);
-        enviar(exchange, json);
+        //pegando o corpo da request que está em json
+        //e transformando na classe Passageiro
+        //para que assim seja possivel a função no repository
+        //ler os valores de passageiro e mandar para o banco
+
+        Passageiro criado = service.criarPassageiro(p);//salvando no banco
+        String json = mapper.writeValueAsString(criado);//<- por isso a função do repository retorna um Passageiro
+        //para que ele consiga ser lida pelo JACKSON e transformar o valor criado em json para que assim seja mandado ao usuário
+
+        enviar(exchange, json);//<- *
     }
 
+
+    // aquela função que a gente manda para o usuário se deu certo ou não está marcada com //<- *
     private void enviar(HttpExchange exchange, String resposta) throws IOException {
         exchange.getResponseHeaders().add("Content-Type", "application/json");
         exchange.sendResponseHeaders(200, resposta.getBytes().length);
