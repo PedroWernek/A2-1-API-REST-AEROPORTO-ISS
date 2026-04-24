53b9c8c8233937fde527b8318d54de9c791217f5
Edumachadoh <dudu.henrique.em@gmail.com>
2026-04-23 23:24:15 -0300

    Add: criacao de AeronaveService

---
+++ src/main/java/br/edu/up/service/AeronaveService.java
@@ -0,0 +1,71 @@
+package br.edu.up.service;
+
+import br.edu.up.model.Aeronave;
+import br.edu.up.repository.AeronaveRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.UUID;
+
+public class AeronaveService {
+    private final AeronaveRepository repo = new AeronaveRepository();
+
+    public Aeronave criarAeronave(Aeronave p) throws Exception {
+
+        if (p.getCapacidadeAssentos() <= 0 || p.getCapacidadeAssentos() > 1000) {
+            throw new Exception("Capacidade de aeronave inválida");
+        }
+        if (p.getTipo() == "Militar" || p.getTipo() == "Guerra"  || p.getTipo().isEmpty()) {
+            throw new Exception("Tipo de aeronave inválido");
+        }
+
+        if (p.getModelo().isEmpty()) {
+            throw new Exception("Modelo de aeronave vazio");
+        }
+
+        p.setId(UUID.randomUUID().toString());
+        repo.salvar(p);
+        return p;
+    }
+
+    public List<Aeronave> listarAeronaves() throws Exception {
+        return repo.listar();
+    }
+
+    public Aeronave buscarAeronavePorId(String id) throws Exception {
+        Aeronave p = repo.buscarPorId(id);
+        if (p == null){
+            throw new Exception("Aeronave não encontrado");
+        }
+        return p;
+    }
+
+    public Aeronave atualizarAeronave(String id, Aeronave p) throws Exception {
+        Aeronave existente = buscarAeronavePorId(id);
+
+        if (p.getCapacidadeAssentos() <= 0 || p.getCapacidadeAssentos() > 1000) {
+            throw new Exception("Capacidade de aeronave inválida");
+        }
+        if (p.getTipo() == "Militar" || p.getTipo() == "Guerra"  || p.getTipo().isEmpty()) {
+            throw new Exception("Tipo de aeronave inválido");
+        }
+
+        if (p.getModelo().isEmpty()) {
+            throw new Exception("Modelo de aeronave vazio");
+        }
+
+        existente.setCapacidadeAssentos(p.getCapacidadeAssentos());
+        existente.setTipo(p.getTipo());
+        existente.setModelo(p.getModelo());
+
+        repo.atualizar(existente);
+        return existente;
+    }
+
+    public Aeronave deletarAeronave(String id) throws Exception {
+        Aeronave p = buscarAeronavePorId(id);
+
+        repo.deletar(id);
+        return p;
+    }
+}
