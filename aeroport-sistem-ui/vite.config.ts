0f8ece711aac58d577a827d54835d2a97ca4319a
Pedro Wernek <pedrowcnetto@gmail.com>
2026-05-22 18:40:01 -0300

    Ensinando o Vite a ler o atalho arroba

--- aeroport-sistem-ui/vite.config.ts
+++ aeroport-sistem-ui/vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from "node:url"
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/aeronave': 'http://localhost:8000',
-      '/voo': 'http://localhost:8000',
-      '/passageiro': 'http://localhost:8000',
-      '/passagem': 'http://localhost:8000'
-    }
-  }
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 })
