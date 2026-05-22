44dfc5e068a8382e5f8b6c125ec1e6ccf8fac557
PEDRO WERNEK CASTILHO NETTO <pedro.netto@cs.up.edu.br>
2026-05-22 19:26:53 -0300

    Corrigindo estilizacao adicionando o plugin do Tailwind v4

--- aeroport-sistem-ui/vite.config.ts
+++ aeroport-sistem-ui/vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from "node:url"
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/aeronave': 'http://localhost:8000',
-      '/voo': 'http://localhost:8000',
-      '/passageiro': 'http://localhost:8000',
-      '/passagem': 'http://localhost:8000'
-    }
-  }
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 })
